50ebe6f9fd71147d93c8095d053067f86fa633e2
Shani <ning830315@gmail.com>
2022-01-05 10:18:20 +0800

    update

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,104 +1,241 @@
 // import { render } from "ejs";
-// import {acc1_btn_close} from '../js/test';
 
 
-// const userDataUrl ='https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON';
 
-// axios.get(userDataUrl).then(function(response){
+// const userDataUrl ='http://10.107.7.26:8080/SecuredLoanWeb/base_data?kind=base_data&custID=4010-091001';
+// let userData = [];
+// axios.get(userDataUrl)
+// .then(function(response){
 
-//   console.log(response.data);
-//   userData = response.data.data;
 //   render();
+//   userData.push(response.data);
 // })
+// console.log(userData);
 
 // try {
-//   fetch('http://sloan.syspower.com.tw/SecuredLoan/base_data?kind=base_data&custID=4010-091000')
+//   fetch('http://10.107.7.26:8080/SecuredLoanWeb/base_data?kind=base_data&custID=4010-091001')
 // } catch (err) {
 //   console.error(err);
 // }
 
+// const userDataUrl ='http://10.107.7.26:8080/SecuredLoanWeb/base_data?kind=base_data&custID=4010-091001';
 
-let userData = [
+// let userData=[]
+// axios.get(userDataUrl)
+// .then(function(response){
+
+//   console.log(response.data);
+//   b = response.data;
+//   render();
+//   return b;
+// })
+// userData.push(b);    
+// a[0] === b;
+// const userAccData ='http://sloan.syspower.com.tw/SecuredLoan/base_data?kind=acc_data&custID=4010-091000';
+
+// axios.get(userAccData).then(function(response){
+
+//   console.log(response.data);
+//   userAccData = response.data.data;
+//   render();
+// })
+// function ajaxCallJsonp1() {
+
+//   $("#msg").html('新增中...');
+
+//   $.ajax({//http://localhost:8080/SecuredLoanWeb/loanservlet ?username=05067113test&password=12345&dstaddr=0937205071&smbody=haha
+//     url: 'http://10.107.7.26:8080/SecuredLoanWeb',
+//     type: 'post',
+//     async: false,
+//     data: { kind: 'base_data', custID: '4010-091000' },
+//     success: function (rtn) {
+//       console.log(' success ' + JSON.stringify(rtn));
+//     },
+//     error: function (rtn) {
+//       console.log('errors' + JSON.stringify(rtn));
+//     }
+//   })
+// }
+// }
+// let userData = [
+//   {
+//     a1: "2000000",
+//     a2: "1900000",
+//     a3: "100000",
+//     a4: "184.21",
+//     a5: "3.00",
+//     custID: "4010-091000",
+//     name: "王大明"
+//   },
+//   {
+//     a1: "3000000",
+//     a2: "2500000",
+//     a3: "500000",
+//     a4: "174.21",
+//     a5: "2.00",
+//     custID: "4010-091001",
+//     name: "王曉明"
+//   }
+
+// ];
+
+let userAccData = [
   {
-    a1: "2000000",
-    a2: "1900000",
-    a3: "100000",
-    a4: "184.21",
-    a5: "3.00",
     custID: "4010-091000",
-    name: "王大明"
+    c1name: "中國信託銀行西松分行",
+    c1: "822-1234567890",
+    c2: "822-734061234567"
   },
   {
-    a1: "3000000",
-    a2: "2500000",
-    a3: "500000",
-    a4: "174.21",
-    a5: "2.00",
     custID: "4010-091001",
-    name: "王曉明"
+    c1name: "中國信託銀行三重分行",
+    c1: "822-1234567888",
+    c2: "822-734061234588"
   }
 
 ];
 
-//登入
-// let login_btn = document.getElementById('user-login');
-// if(login_btn){
-//   login_btn.addEventListener('click', userLogin ,false);
-// }
+let stockData = [
+  {
+    loanTOValue: "60",
+    pettionUsedMoney: "0",
+    stockName: "南　亞",
+    ableLoanQty: "0",
+    is50: true,
+    pettionUsedQty: "0",
+    riskRatio: "",
+    num: "",
+    tradeUnit: "1000",
+    riskQty: "999",
+    lastPrice: "85.60",
+    stockID: "1303"
+  },
+  {
+    loanTOValue: "60",
+    pettionUsedMoney: "0",
+    stockName: "台積電",
+    ableLoanQty: "0",
+    is50: true,
+    pettionUsedQty: "0",
+    riskRatio: "",
+    num: "",
+    tradeUnit: "1000",
+    riskQty: "999",
+    lastPrice: "612.00",
+    stockID: "2330"
+  },
+  {
+    loanTOValue: "60",
+    pettionUsedMoney: "0",
+    stockName: "泰茂",
+    ableLoanQty: "500",
+    is50: false,
+    pettionUsedQty: "0",
+    riskRatio: "",
+    num: "",
+    tradeUnit: "1000",
+    riskQty: "30",
+    lastPrice: "13.45",
+    stockID: "2884"
+  },
+  {
+    loanTOValue: "60",
+    pettionUsedMoney: "0",
+    stockName: "玉山金",
+    ableLoanQty: "300",
+    is50: true,
+    pettionUsedQty: "0",
+    riskRatio: "",
+    num: "",
+    tradeUnit: "1000",
+    riskQty: "999",
+    lastPrice: "27.40",
+    stockID: "2230"
+  }
 
-// function userLogin  (event) {
-//   let userAcc = document.getElementById('user-account');
-//   let userPsd = document.getElementById('user-password');
-//   let userId = userData.some(x => x.custID == userAcc.value);
+]
 
-//   // console.log(userId);
-//   if (userId){
-//     if (userPsd.value.length >=4){
-//       window.location.href = 'ioan.html';
-//     }else{
-//       userPsd.value="";
-//       alert('密碼重新輸入')
-//     }
-//   }else{
-//     userAcc.value="";
-//     userPsd.value="";
-//     alert('請重新輸入')
-//   }
+let userStockData = [
+  {
+    num: 2,
+    stocks: [
+      {
+        ableLoanQty: "300000",
+        custID: "4010-091000",
+        transDate: "20211126",
+        stockID: "2330"
+      },
+      {
+        ableLoanQty: "300000",
+        custID: "4010-091000",
+        transDate: "20211126",
+        stockID: "1303"
+      },
 
-// }
-let login_btn = document.getElementById('user-login');
-if (login_btn) {
-  login_btn.addEventListener('click', userLogin, false);
-}
-function userLogin() {
+    ]
+  },
+  {
+    num: 2,
+    stocks: [
+      {
+        ableLoanQty: "500000",
+        custID: "4010-091001",
+        transDate: "20211126",
+        stockID: "2230"
+      },
+      {
+        ableLoanQty: "300000",
+        custID: "4010-091001",
+        transDate: "20211126",
+        stockID: "2884"
+      }
+    ]
+  }
+
+
+]
+
+
+  //登入
+  let login_btn = document.getElementById('user-login');
   let userAcc = document.getElementById('user-account');
   let userPsd = document.getElementById('user-password');
-  let userId = userData.findIndex(x => x.custID === userAcc.value);
-
-  if (userId >= 0) {
-    let user = userData[userId].custID;
-    localStorage.setItem("userid", user);
-    // console.log(user)
-    if (userPsd.value.length >= 4) {
-      window.location.href = 'ioan.html';
-    } else {
-      userPsd.value = "";
-      alert('密碼重新輸入')
-    }
-    return user;
-  } else {
-    userAcc.value = "";
-    userPsd.value = "";
-    alert('請重新輸入')
+  if(login_btn){
+    userPsd.addEventListener('keypress',function(e){
+      if(e.key=='Enter'){
+        userLogin();
+      }
+    })
   }
-}
-let login_user = localStorage.getItem("userid");
+  if (login_btn) {
+    login_btn.addEventListener('click', userLogin, false);
+  }
+  function userLogin() {
+    let userId = userData.findIndex(x => x.custID === userAcc.value);
+  
+    if (userId >= 0) {
+      let user = userData[userId].custID;
+      sessionStorage.setItem("userid", user);
+      if (userPsd.value.length >= 4) {
+        window.location.href = 'loan.html';
+      } else {
+        userPsd.value = "";
+        alert('密碼重新輸入')
+      }
+      return user;
+    } else {
+      userAcc.value = "";
+      userPsd.value = "";
+      alert('請重新輸入')
+    }
+  }
+  //透過localstorage存的帳號找到是userData哪筆資料(index)
+  let login_user = sessionStorage.getItem("userid");
+  let user_index = userData.findIndex(x => x.custID === login_user)
 
 //借款申請使用者資料代入
-if (window.location.pathname == '/ioan.html') {
+if (window.location.pathname == '/loan.html') {
 
-  let user_index = userData.findIndex(x => x.custID === login_user)
-  // console.log(user_index);
   let userAccInfo = document.getElementById('user-accountInfo');
   let userName = document.getElementById('user-name');
   let user_a1 = document.getElementById('a1');
@@ -130,86 +267,235 @@ if (window.location.pathname == '/ioan.html') {
   }
 
   //借款金額and目的設定
-  let ioanNum = document.getElementById('ioanNum');
-  // console.log(parseInt(ioanNum));
-  let ioanReason = document.getElementById('ioanReason');
-  // console.log(ioanReason) r1
-  let clean_btn = document.getElementById('ioan-btn-clean');
-  let ioan_btn = document.getElementById('ioan-btn-submit');
-
+  let loanNum = document.getElementById('loanNum');
+  let loanReason = document.getElementById('loanReason');
+  let clean_btn = document.getElementById('loan-btn-clean');
+  let loan_btn = document.getElementById('loan-btn-submit');
+ 
+   
   //清除按鈕設定
   if (clean_btn) {
     clean_btn.addEventListener('click', function () {
-      ioanNum.value = '';
-      ioanReason.selectedIndex = 0;
+      loanNum.value = '';
+      loanReason.selectedIndex = 0;
     })
   }
   //申請按鈕設定
-  if (ioan_btn) {
-    ioan_btn.addEventListener('click', ioanSubmit, false);
+  if (loan_btn) {
+    loan_btn.addEventListener('click', loanSubmit, false);
   }
-  // console.log(user_a1.value)
   //a1,a2從字串100,000,000轉回數字100000
   let data_a1 = parseInt(user_a1.value.split(',').join(''));
   let data_a2 = parseInt(user_a2.value.split(',').join(''));
-  // console.log(typeof data_a1)
-  // console.log(typeof data_ioan)
 
   //借款申請判斷
-  function ioanSubmit() {
-    let data_ioanNum = parseInt(ioanNum.value);
-    localStorage.setItem("ioanNum", data_ioanNum.toString());
-    localStorage.setItem("ioanReason", ioanReason.value)
-    if (data_a1 - data_a2 >= data_ioanNum && data_ioanNum >= 10000) {
+  function loanSubmit() {
+     //借款目的為其他，值為輸入項內容
+     if(loanReason.value == 'r6'){
+      sessionStorage.setItem("loanReason", loanReason_input.value )  
+  }else{
+    sessionStorage.setItem("loanReason", loanReason.value)
+  }
+  
+    let data_loanNum = parseInt(loanNum.value);
+    sessionStorage.setItem("loanNum", data_loanNum.toString());
+    if (data_a1 - data_a2 >= data_loanNum && data_loanNum >= 10000) {
       window.location.href = 'acc_info.html';
-    } else if (data_a1 - data_a2 < data_ioanNum) {
-      window.location.href = 'index.html';
+    } else if (data_a1 - data_a2 < data_loanNum) {
+      alert('超出可借金額需增加擔保品');
+      window.location.href = 'acc_overloan.html';
     } else {
       alert('wrong')
-      ioanNum.value = '';
+      loanNum.value = '';
     }
   }
 
 }
-//按鈕關閉acc1,acc2
-// let acc1_btn_close = document.getElementById('acc1-btn-close');
-// let acc2_btn_close = document.getElementById('acc2-btn-close');
-// console.log(acc1_btn_close)
-// function close_accInfo () {
-//   let acc1_info =document.getElementById('acc1-info');
-//   let acc2_info =document.getElementById('acc2-info');
-//   // console.log(this.currentTarget);
-//   console.log(this.target);
 
-//   // if(e.target == acc1_btn_close){acc1_info.style.display ='none'}; 
-//   // if(e.target == acc2_btn_close){acc2_info.style.display ='none'}; 
-// }
-// acc1_btn_close.addEventListener('click', function(){
-//   console.log('fff')
-
-//   // return false;
-// });
 //表格內資料代入
-let s_ioanNum = localStorage.getItem('ioanNum');
-let s_ioanReason = localStorage.getItem('ioanReason');
-let acc_ioanNum = document.querySelectorAll('[name="acc-ioanNum"]');
-let acc_ioanReason = document.getElementsByName('acc-ioanReason');
+let s_loanNum = sessionStorage.getItem('loanNum');
+let s_loanReason = sessionStorage.getItem('loanReason');
+let acc_loanNum = document.querySelectorAll('[name="acc-loanNum"]');
+let acc_loanReason = document.getElementsByName('acc-loanReason');
 let acc_a5 = document.getElementsByName('a5');
 
-// console.log(acc_ioanNum);
-// console.log(acc_ioanReason);
-// console.log(acc_a5);
-// acc_ioanNum.textContent = s_ioanNum;
-for (let iN of acc_ioanNum) {
-  iN.textContent = s_ioanNum;
+for (let iN of acc_loanNum) {
+  iN.textContent = s_loanNum;
 }
-for (let iR of acc_ioanReason) {
-  iR.textContent = s_ioanReason;
+for (let iR of acc_loanReason) {
+  iR.textContent = s_loanReason;
+}
+for (let a5 of acc_a5) {
+  a5.textContent = `${userData[user_index].a5}%`;
 }
 
-let user_index = userData.findIndex(x => x.custID === login_user);
+//帳戶資料代入
 
-let acc1_btn_close = document.getElementById('acc1-btn-close');
-acc1_btn_close.addEventListener('click', function () {
-  console.log('fff')
-});
+if (window.location.pathname == '/acc_info.html' || window.location.pathname == '/check_overloan.html') {
+  let user_acc1 = document.getElementById('user-acc1');
+  let user_acc2 = document.getElementById('user-acc2');
+
+  user_acc1.textContent = userAccData[user_index].c1;
+  user_acc2.textContent = userAccData[user_index].c2;
+
+}
+
+
+
+let over_loanNum = document.getElementById('loanNum-over');
+if (window.location.pathname == '/acc_overloan.html') {
+  over_loanNum.textContent = s_loanNum;
+
+  //使用者股票資料代入
+
+  let stock_list = document.getElementById('stock-list');
+  let user_stock_arr = userStockData[user_index].stocks;
+  let stock_len = userStockData[user_index].num;
+  let stocklist_str = '';
+  // for (let i = 0; i < stock_len; i++) {  
+  for (let us_info in user_stock_arr) {
+    let stock_index = stockData.findIndex(x => x.stockID == user_stock_arr[us_info].stockID);
+
+    // 要插入的字串
+    let stocklist_content = `
+      <tr>
+      <td name="stock-id">${user_stock_arr[us_info].stockID}</td>
+      <td>${stockData[stock_index].stockName}</td>
+      <td name="stock-qty">${user_stock_arr[us_info].ableLoanQty / 1000}</td>
+      <td name="loan-val">${stockData[stock_index].loanTOValue}%</td>
+      <td><input type="text" name="qty" class="border-0"></td>
+      </tr>
+      `;
+    stocklist_str += stocklist_content;
+
+  }
+  stock_list.innerHTML = stocklist_str;
+
+  //擔保品張數
+  let qtys = document.getElementsByName('qty');
+  let stock_id = document.getElementsByName('stock-id');
+  let stock_ableqty = document.getElementsByName('stock-qty');
+
+  let user_stockid_arr = [];
+  let user_stockableqty_arr = [];
+  //lastprice array
+  let lp_arr = [];
+  //loantovalue array
+  let lv_arr = [];
+  //stockname array
+  let ln_arr = [];
+  for (let i = 0; i < stock_len; i++) {
+    let a = stock_id[i].textContent
+    user_stockid_arr.push(a);
+    let b = stockData.findIndex(x => x.stockID == user_stockid_arr[i]);
+    let c = stock_ableqty[i].textContent
+    user_stockableqty_arr.push(c);
+
+    lp_arr.push(stockData[b].lastPrice);
+    lv_arr.push(stockData[b].loanTOValue);
+    ln_arr.push(stockData[b].stockName);
+  }
+  let qty_arr = [];
+  for (let i = 0; i < qtys.length; i++) {
+    qty_arr.push(0);
+  }
+  let loan_quota = document.getElementById('loan-quota');
+  let overloan_submit = document.getElementById('overloan-btn-submit');
+  qtys.forEach((e) => {
+    e.addEventListener('change', () => {
+      const qtynodes_arr = Array.from(qtys);
+      qty_arr[qtynodes_arr.indexOf(e)] = e.value;
+      let user_stockNum = user_stockableqty_arr[qtynodes_arr.indexOf(e)];
+      if (parseInt(user_stockNum) < parseInt(qty_arr[qtynodes_arr.indexOf(e)])) {
+        console.log(user_stockNum - qty_arr[qtynodes_arr.indexOf(e)])
+        alert('超出庫存張數，請重新填寫')
+        e.value = '';
+      } else {
+        let loan_quota_total = 0;
+        for (let i = 0; i < qty_arr.length; i++) {
+
+          let loan_sum = (lp_arr[i]) * (lv_arr[i]) / 100 * (qty_arr[i]) * 1000;
+          loan_quota_total = loan_quota_total + loan_sum;
+          loan_quota.textContent = Math.round(loan_quota_total);
+          let prduct_list = { stockId: user_stockid_arr, stockName: ln_arr, productQty: qty_arr }
+         
+          sessionStorage.setItem("userLoan", JSON.stringify(prduct_list));
+        }
+        sessionStorage.setItem("loan_quota_total", loan_quota_total);
+      }
+
+    })
+
+  })
+
+  overloan_submit.addEventListener("click", () => {
+    let loanQuota_total = sessionStorage.getItem('loan_quota_total');
+    let loanNum = sessionStorage.getItem('loanNum')
+
+    if (loanQuota_total - parseInt(loanNum) >= 0) {
+      window.location.href = 'check_overloan.html'
+    } else {
+      console.log(loanQuota_total - parseInt(loanNum))
+      alert('融通額度不足');
+    }
+
+  })
+
+}
+if (window.location.pathname == '/check_overloan.html' || window.location.pathname == '/acc_overloan.html') {
+  let user_loan = JSON.parse(sessionStorage.getItem("userLoan"));
+  let user_loan_list = document.getElementsByClassName('user-loan-list');
+  // let count_empty_qty = user_loan.productQty.filter(x => x==='' || x=='0' || x === 0 || x==null).length
+  let empty_arr = user_loan.productQty.filter(x => x==='' || x=='0' || x === 0 || x ==null);
+  // user_loan.productQty[count_empty_index] = 0
+  // console.log( count_empty_qty );
+  // console.log( count_empty_index );
+
+
+  let loanlist_str = '';
+  for (let i = 0; i < user_loan.productQty.length; i++) {
+    if (user_loan.productQty[i]===""|| user_loan.productQty[i]==="0"||user_loan.productQty[i]===0||user_loan.productQty[i]===null) {
+
+      let loanlist_content = `
+        <tr class="d-none">
+        <td>${user_loan.stockId[i]}</td>
+        <td>${user_loan.stockName[i]}</td>
+        <td class="qty-num">${user_loan.productQty[i]}</td>
+        </tr>
+        `;
+      loanlist_str += loanlist_content
+
+    }
+    else {
+      let loanlist_content = `
+        <tr>
+        <td>${user_loan.stockId[i]}</td>
+        <td>${user_loan.stockName[i]}</td>
+        <td class="qty-num">${user_loan.productQty[i]}</td>
+        </tr>
+        `;
+      loanlist_str += loanlist_content
+
+    }
+  }
+
+
+
+  // }  
+
+  // if (window.location.pathname == '/check_overloan.html'){
+  console.log(loanlist_str);
+  for (let el of user_loan_list) {
+    el.innerHTML = loanlist_str
+  }
+  let qtyNum = document.getElementsByClassName('qty-num');
+  let qtys_sum = document.getElementsByClassName('product-total');
+  let total_data = 0
+  for (let i = 0; i < user_loan.productQty.length; i++) {
+    total_data = total_data + parseInt(qtyNum[i].textContent);
+    for (let c of qtys_sum) {
+      c.textContent = total_data;
+    }
+  }
+
+}
